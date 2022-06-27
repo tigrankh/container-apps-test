cd1397cbac4d5c52389bfaa5be6c8d8efdb42a32
Tigran Khazhakyan <tigran.khazhakyan@gmail.com>
2022-06-27 14:08:52 +0400

    removed log

--- consumer/app/api.py
+++ consumer/app/api.py
@@ -8,7 +8,6 @@ QUEUE_NAME = os.environ.get("SB_QUEUE_NAME")
 servicebus_client = ServiceBusClient.from_connection_string(conn_str=CONN_STR, logging_enable=True)
 
 with servicebus_client:
-    print("Waiting on messages...")
     with servicebus_client.get_queue_receiver(queue_name=QUEUE_NAME) as receiver:
         msg = next(receiver)
         receiver.complete_message(msg)
